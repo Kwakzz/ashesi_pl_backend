4819b1b2c916e6563b7369cd05bbb984318c2f0b
Kwakzz <kwakuosafo20@gmail.com>
2024-01-10 19:22:10 +0000

    set debug to true

--- ashesi_premier_league/settings.py
+++ ashesi_premier_league/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u8yf91ekro16$hl3s10-@x)955l)t6$4cihm8=3105s@6h0*)7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
