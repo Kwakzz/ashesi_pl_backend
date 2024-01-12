4744c46cf9037036e909b8a25d088c7ca1ee39f1
Kwakzz <kwakuosafo20@gmail.com>
2024-01-12 01:36:01 +0000

    nothing

--- ashesi_premier_league/settings.py
+++ ashesi_premier_league/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'ashesi_premier_league.wsgi.application'
 CORS_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:62894',
+    'http://localhost:53953',
 ]
 
 # Database
